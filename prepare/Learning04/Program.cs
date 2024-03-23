06fb3b41bfe2d07fbafc603ee2a3113726381408
sylvanhub <sylvesterodeh9@gmail.com>
2024-03-23 05:40:30 +0100

    few adjustment added again

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,15 +5,15 @@ class Program
     static void Main(string[] args)
     {
         // Create a base "Assignment" object
-        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Assignment a1 = new Assignment("Aderibigbe John", "Multiplication");
         Console.WriteLine(a1.GetSummary());
 
         // Now create the derived class assignments
-        MathAssignment a2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        MathAssignment a2 = new MathAssignment("Itunuoluwa Oluwaseun", "Fractions", "7.3", "8-19");
         Console.WriteLine(a2.GetSummary());
         Console.WriteLine(a2.GetHomeworkList());
 
-        WritingAssignment a3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        WritingAssignment a3 = new WritingAssignment("Samson Cole", "Politics", "Causes of poverty in Nigeria");
         Console.WriteLine(a3.GetSummary());
         Console.WriteLine(a3.GetWritingInformation());
     }
